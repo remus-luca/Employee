3d6fbe3aef5638ccfc86364e551d2b1b1e4b76c2
remus-luca <lucaremusmarian@yahoo.com>
2023-01-13 21:19:35 +0200

    update modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  ngOnInit(): void {}
   title = 'Employee';
+  @ViewChild('tempButton') buttontemp: any;
+
+  educationOptions = [
+    '10th pass',
+    'diploma',
+    'graduate',
+    'post graduate',
+    'PhD',
+  ];
+
+  ngAfterViewInit(): void {
+    this.buttontemp.nativeElement.click();
+  }
 }
